7c82a13e0d7c3a6226bf36dfdff05bef64dbfb55
SwachandaRoy <96686803+SwachandaRoy@users.noreply.github.com>
2025-12-25 14:32:22 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app-background'>
+      <header className="App-header p-10">
+        <h5 className='text-blue-50'>Coding Conf</h5>
+        <h1 className='text-red-500'>HI THERE</h1>
+       <h1>
+        Hello World
+       </h1>
+      </header>
+      </div>
+   </div>
+  );
+}
+
+export default App;
